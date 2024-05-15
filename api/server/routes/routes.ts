932e65c403601e04e9c49733104f890abee0547f
Julio Cesar <juliomtd23@gmail.com>
2024-05-15 00:05:15 -0300

    .

--- api/server/routes/routes.ts
+++ api/server/routes/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { addUserController, allUsersController, editUserController, deleteUserController, singUpController, signInController } from "../../controllers/controllers";
+import {Response} from "express";
+
 
 const router = Router();
 /* Management system */
 router.post("/adduser", addUserController);
+
+router.get("/", (res:Response) => {
+  res.send("HELLO")
+})
 
 router.get("/allusers", allUsersController);
 
