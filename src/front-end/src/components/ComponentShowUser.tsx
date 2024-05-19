94cdf14dc86216f442434aab3ca657d65486c2b7
Julio Cesar <juliomtd23@gmail.com>
2024-05-19 16:07:48 -0300

    add name

--- src/front-end/src/components/ComponentShowUser.tsx
+++ src/front-end/src/components/ComponentShowUser.tsx
@@ -79,7 +79,7 @@ export const ComponentShowUser = (props: any) => {
         <div className="accordion-item">
           <h2 className="accordion-header">
             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-              FUNCIONÁRIO
+              {name}
             </button>
           </h2>
           <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
